cfa3cfd736006f39a5642f6d7251379095b2192d
jpedryc <jan.pedryc@gmail.com>
2024-12-10 23:27:43 +0100

    Add Day 10, Task 2

--- day_10/main.go
+++ day_10/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"day10/task1"
+	"day10/task2"
 	"fmt"
 )
 
@@ -9,4 +10,6 @@ func main() {
 	fmt.Println("Day 10 Results")
 	task1Result := task1.Task1()
 	fmt.Println("Task1 result:", task1Result) // 472
+	task2Result := task2.Task2()
+	fmt.Println("Task2 result:", task2Result) // 969
 }
